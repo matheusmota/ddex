a48591ab0e09670af9f973f2b1bd6f960f5067de
smota.matheus@gmail.com <smota.matheus@gmail.com@9765d10f-c9b9-9390-64f8-9496d5dcfe03>
2011-06-01 21:54:52 +0000

    uploading jar for PDF support (iText)

--- trunk/src/java/br/unicamp/ic/lis/ddex/textddex/builder/ITextBuilder.java
+++ trunk/src/java/br/unicamp/ic/lis/ddex/textddex/builder/ITextBuilder.java
@@ -1,13 +1,13 @@
 package java.br.unicamp.ic.lis.ddex.textddex.builder;
 
 import java.br.unicamp.ic.lis.ddex.textddex.TextDocumentProperties;
-import java.br.unicamp.ic.lis.ddex.textddex.parts.CharSequence;
-import java.br.unicamp.ic.lis.ddex.textddex.parts.Paragraph;
-
+import java.br.unicamp.ic.lis.ddex.textddex.part.CharSequence;
+import java.br.unicamp.ic.lis.ddex.textddex.part.Paragraph;
+import java.br.unicamp.ic.lis.ddex.textddex.part.TextObject;
 
 /**
  * @author Matheus Mota
- * @category Interface
+ * 
  */
 
 public interface ITextBuilder {
@@ -15,7 +15,7 @@ public interface ITextBuilder {
 	/**
 	 * This method is called when: Director found the begin of a text document.
 	 * 
-	 * @param properties
+	 * @param textFileProperties
 	 *            Properties of the text file, like full path, size, number of
 	 *            letters, number of images, number of styles, mime-type etc.
 	 * 
@@ -35,9 +35,11 @@ public interface ITextBuilder {
 
 	/**
 	 * This method is called when: Director found a char sequence into a
-	 * paragraph. CharSequences are usually called when the actual properties of a charSequence is modified
-	 *  
-	 * @param charSequence holds the content and properties of a charSequence
+	 * paragraph. CharSequences are usually called when the actual properties of
+	 * a charSequence is modified
+	 * 
+	 * @param charSequence
+	 *            holds the content and properties of a charSequence
 	 */
 
 	public void foundCharSequenceBegin(CharSequence charSequence);
@@ -46,16 +48,22 @@ public interface ITextBuilder {
 	 * This method is called when: Director found a image into the char sequence
 	 * into a paragraph
 	 * 
-	 * @param
+	 * @param textObject
+	 *            holds the properties of the founded object and it self. For
+	 *            example, if its a image, the object type will be IMAGE and the
+	 *            method getAsImage(); will return the image itself.
 	 */
 
-	public void foundObject();
+	public void foundObject(TextObject textObject);
 
 	/**
 	 * This method is called when: Director found the end of a char sequence
-	 * into a paragraph
+	 * into a paragraph. For instance, if you are creating aXML, this is a good
+	 * time to end your tag (</endcharsequence>). CharSequences are usually
+	 * called when the actual properties of a charSequence is modified
 	 * 
-	 * @param
+	 * @param charSequence
+	 *            holds the content and properties of a charSequence
 	 */
 
 	public void foundCharSequenceEnd(CharSequence charSequence);
@@ -63,13 +71,21 @@ public interface ITextBuilder {
 	/**
 	 * This method is called when: Director found the end of a paragraph
 	 * 
-	 * @param
+	 * @param paragraph
+	 *            Represents the paragraph it-self, having the text content and
+	 *            some properties, such as style orientation etc.
 	 */
+
 
 	public void foundParagraphEnd(Paragraph paragraph);
 
 	/**
-	 * This method is called when: Director found the end of a text document
+	 * This method is called when: Director found the end of a text document.
+	 * 
+	 * @param textFileProperties
+	 *            Properties of the text file, like full path, size, number of
+	 *            letters, number of images, number of styles, mime-type etc.
+	 * 
 	 */
 	public void foundTextDocumentEnd(TextDocumentProperties textFileProperties);
 
