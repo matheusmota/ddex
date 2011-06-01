732f5e37c226f39a97feb124b1772a87e6deea57
smota.matheus@gmail.com <smota.matheus@gmail.com@9765d10f-c9b9-9390-64f8-9496d5dcfe03>
2011-06-01 22:08:06 +0000

    changes on the doc reader and image support (by the util package)

--- trunk/src/java/br/unicamp/ic/lis/ddex/textddex/TextDocumentProperties.java
+++ trunk/src/java/br/unicamp/ic/lis/ddex/textddex/TextDocumentProperties.java
@@ -1,12 +1,23 @@
-/**
- * 
- */
 package java.br.unicamp.ic.lis.ddex.textddex;
 
 /**
  * @author matheus
- *
+ * 
  */
 public class TextDocumentProperties {
+
+	private String filePath = null;
+
+	public TextDocumentProperties(String fPath) {
+		this.filePath = fPath;
+
+	}
+
+	/**
+	 * @return the file path
+	 */
+	public String getFilePath() {
+		return filePath;
+	}
 
 }
---
+++ trunk/src/java/br/unicamp/ic/lis/ddex/util/image/ByteArrayToFileImage.java
@@ -0,0 +1,29 @@
+package java.br.unicamp.ic.lis.ddex.util.image;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ByteArrayToFileImage {
+
+	public ByteArrayToFileImage() {
+
+	}
+
+	public OutputStream convert(String path, byte[] content) {
+
+		OutputStream stream = null;
+		try {
+			stream = new FileOutputStream(path);
+			stream.write(content);
+
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return stream;
+	}
+}
