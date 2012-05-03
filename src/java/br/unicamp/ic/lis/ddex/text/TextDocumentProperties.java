ba46de31157acfc9a947157ed66df41a8ff5e6e2
smota.matheus <smota.matheus@9765d10f-c9b9-9390-64f8-9496d5dcfe03>
2012-05-03 00:41:59 +0000

    text code clean

---
+++ src/java/br/unicamp/ic/lis/ddex/text/TextDocumentProperties.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package java.br.unicamp.ic.lis.ddex.text;
+
+/**
+ * @author matheus
+ *
+ */
+public class TextDocumentProperties {
+	
+	
+	public boolean pagesIndication = false;
+	public String fullpath;
+	public int filesize;
+	public long numberOfWords;
+	public long numberOfImages;
+	
+	
+	
+}
